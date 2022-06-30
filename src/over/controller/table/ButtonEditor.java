46ddd0d6cac8f2fa452f560818e5a60ed319b0b9
Overload-Inc <overload.inc.mx@gmail.com>
2022-06-30 01:55:36 -0500

    Add JavaDoc comments

---
+++ src/over/controller/table/ButtonEditor.java
@@ -0,0 +1,33 @@
+package over.controller.table;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * <code>ButtonEditor</code> class.
+ * @author Overload Inc.
+ * @version 1.0, 19 Jun 2022
+ */
+public class ButtonEditor extends DefaultCellEditor {
+    private JButton button = new JButton();
+    private String label;
+
+    /**
+     *
+     * @param checkBox
+     */
+    public ButtonEditor(JCheckBox checkBox) {
+        super(checkBox);
+        button.addActionListener(event -> JOptionPane.showMessageDialog(null,"Do you want to modify this line?"));
+    }
+
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        label = (value == null) ? "Modify" : value.toString();
+        button.setText(label);
+        return button;
+    }
+
+    public Object getCellEditorValue() {
+        return new String(label);
+    }
+}
---
+++ src/over/controller/table/ButtonRenderer.java
@@ -0,0 +1,25 @@
+package over.controller.table;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+/**
+ * <code>ButtonRenderer</code> class.
+ * @author Overload Inc.
+ * @version 1.0, 19 Jun 2022
+ */
+public class ButtonRenderer extends JButton implements TableCellRenderer {
+
+    /**
+     *
+     */
+    public ButtonRenderer() {
+        setOpaque(true);
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        setText((value == null) ? "Modify" : value.toString());
+        return this;
+    }
+}
