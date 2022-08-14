7768a36e9454230082e5de168fb19203f8913fcc
Overload-Inc <overload.inc.mx@gmail.com>
2022-08-13 23:54:43 -0500

    Add JButton components to the JTable

--- src/over/core/controller/table/ButtonRenderer.java
+++ src/over/core/controller/table/ButtonRenderer.java
@@ -1,5 +1,7 @@
 package over.core.controller.table;
 
+import over.config.Configurator;
+
 import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
@@ -19,7 +21,13 @@ public class ButtonRenderer extends JButton implements TableCellRenderer {
     }
 
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        setText((value == null) ? "Modify" : value.toString());
+        switch (column) {
+            case 5: setText(Configurator.getConfigurator().getProperty("playColumn")); break;
+            case 6: setText(Configurator.getConfigurator().getProperty("stopColumn")); break;
+            case 7: setText(Configurator.getConfigurator().getProperty("deleteColumn")); break;
+            default: value.toString();
+        }
+
         return this;
     }
 }
