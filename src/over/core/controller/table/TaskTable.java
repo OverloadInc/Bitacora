cbb47362fa3224584a494b67c6e6fdd82774cd16
Overload-Inc <overload.inc.mx@gmail.com>
2022-08-13 23:04:39 -0500

    Update JavaDoc comments

--- src/over/core/controller/table/TaskTable.java
+++ src/over/core/controller/table/TaskTable.java
@@ -1,15 +1,12 @@
 package over.core.controller.table;
 
 import over.config.Configurator;
-import over.core.model.Task;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 /**
- * <code>TaskTable</code> class.
+ * <code>TableController</code> class.
  * @author Overload Inc.
  * @version 1.0, 19 Jun 2022
  */
@@ -18,7 +15,6 @@ public final class TaskTable {
     private DefaultTableModel tableModel;
     private JTable taskTable;
     private Object colums[];
-    private int index;
 
     /**
      * Class constructor.
@@ -45,38 +41,14 @@ public final class TaskTable {
         tableModel = new DefaultTableModel(0, 0);
         tableModel.setColumnIdentifiers(colums);
 
-        taskTable.setModel(tableModel);
+        this.taskTable.setModel(tableModel);
 
-        taskTable.getColumn(Configurator.getConfigurator().getProperty("playColumn")).setCellRenderer(new ButtonRenderer());
-        taskTable.getColumn(Configurator.getConfigurator().getProperty("playColumn")).setCellEditor(new ButtonEditor(new JCheckBox()));
-        taskTable.getColumn(Configurator.getConfigurator().getProperty("stopColumn")).setCellRenderer(new ButtonRenderer());
-        taskTable.getColumn(Configurator.getConfigurator().getProperty("stopColumn")).setCellEditor(new ButtonEditor(new JCheckBox()));
-        taskTable.getColumn(Configurator.getConfigurator().getProperty("deleteColumn")).setCellRenderer(new ButtonRenderer());
-        taskTable.getColumn(Configurator.getConfigurator().getProperty("deleteColumn")).setCellEditor(new ButtonEditor(new JCheckBox()));
-
-        refreshTable();
-    }
-
-    /**
-     * Adds a new user task to the <code>JTable</code>.
-     * @param taskName the user task name.
-     */
-    public void addTask(String taskName) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime localTime = LocalTime.now();
-
-        Task task = new Task();
-        task.setId(index++);
-        task.setName(taskName);
-        task.setStartTime(localTime);
-
-        tableModel.addRow(new Object[]{task.getId(), task.getName(), dtf.format(task.getStartTime()), "", ""});
-    }
-
-    /**
-     * Updates the <code>JTable</code>'s information with current user tasks.
-     */
-    public void refreshTable() {
+        this.taskTable.getColumn(Configurator.getConfigurator().getProperty("playColumn")).setCellRenderer(new ButtonRenderer());
+        this.taskTable.getColumn(Configurator.getConfigurator().getProperty("playColumn")).setCellEditor(new ButtonEditor(new JCheckBox()));
+        this.taskTable.getColumn(Configurator.getConfigurator().getProperty("stopColumn")).setCellRenderer(new ButtonRenderer());
+        this.taskTable.getColumn(Configurator.getConfigurator().getProperty("stopColumn")).setCellEditor(new ButtonEditor(new JCheckBox()));
+        this.taskTable.getColumn(Configurator.getConfigurator().getProperty("deleteColumn")).setCellRenderer(new ButtonRenderer());
+        this.taskTable.getColumn(Configurator.getConfigurator().getProperty("deleteColumn")).setCellEditor(new ButtonEditor(new JCheckBox()));
     }
 
     /**
@@ -84,7 +56,7 @@ public final class TaskTable {
      * @return the <code>TaskTable</code> instance.
      */
     public static TaskTable getInstance() {
-        if(instance == null)
+        if (instance == null)
             instance = new TaskTable();
 
         return instance;
@@ -95,6 +67,14 @@ public final class TaskTable {
      * @return the <code>JTable</code> instance.
      */
     public JTable getTaskTable() {
-        return this.taskTable;
+        return taskTable;
+    }
+
+    /**
+     * Gets the user task table's <code>DefaultTableModel</code> instance.
+     * @return the current <code>DefaultTableModel</code> instance.
+     */
+    public DefaultTableModel getTableModel() {
+        return tableModel;
     }
 }
