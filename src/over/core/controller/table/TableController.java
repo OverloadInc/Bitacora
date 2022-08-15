abb32b3b9f04d60322b5d015e4ebe91f2e90896d
Overload-Inc <overload.inc.mx@gmail.com>
2022-08-14 23:37:42 -0500

    Add play and stop functionalities to the JButtons

--- src/over/core/controller/table/TableController.java
+++ src/over/core/controller/table/TableController.java
@@ -1,13 +1,16 @@
 package over.core.controller.table;
 
 import over.core.gui.Bitacora;
+import over.core.model.pojo.Status;
 import over.core.model.pojo.Task;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.sql.Date;
+import java.time.Duration;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 /**
  * <code>TableController</code> class.
@@ -18,6 +21,7 @@ public class TableController {
     private JTable taskTable;
     private DefaultTableModel tableModel;
     private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static ArrayList<Task> taskList = new ArrayList<>();
 
     /**
      * Class constructor.
@@ -25,6 +29,19 @@ public class TableController {
     public TableController() {
         taskTable = TaskTable.getInstance().getTaskTable();
         tableModel = TaskTable.getInstance().getTableModel();
+    }
+
+    /**
+     * Searches for a specific task inside a list of <code>Task</code> objects.
+     * @param id the <code>id</code> of the task required.
+     * @return the <code>Task</code> object; <code>null</code> otherwise.
+     */
+    public Task find(int id) {
+        for (Task task : taskList)
+            if (task.getId() == id)
+                return task;
+
+        return null;
     }
 
     /**
@@ -41,11 +58,9 @@ public class TableController {
             task.setFirstTime(true);
             task.setEffectiveTime(LocalTime.of(00, 00, 00));
             task.setDate(new Date(System.currentTimeMillis()));
-
             task.add();
 
             refreshTable(1);
-            Bitacora.addMessage(name);
         }
         else
             JOptionPane.showMessageDialog(null, "User task not valid!");
@@ -61,7 +76,6 @@ public class TableController {
         if(selectedRow >= 0 && rowCount >= 1) {
             Task task = new Task();
             task.setId((int)tableModel.getValueAt(selectedRow, 0));
-
             task.delete();
 
             refreshTable(2);
@@ -70,16 +84,71 @@ public class TableController {
             JOptionPane.showMessageDialog(null, "Select a valid task");
     }
 
-    public void updateTask() {
-        System.out.println("Update task");
+    public void updateTask(Task task) {
+        if (task != null)
+            task.update();
     }
 
+    //TODO Implementation of effective time mechanism
     public void play() {
-        System.out.println("Play task");
+        int selectedRow = taskTable.getSelectedRow();
+        int rowCount = taskTable.getRowCount();
+
+        if(selectedRow >= 0 && rowCount >= 1) {
+            Task task = find((int)taskTable.getValueAt(selectedRow, 0));
+
+            if (task.isFirstTime() && task.getStatus() == Status.PLAY) {
+                task.setStatus(Status.PAUSE);
+                task.setStartTime(LocalTime.now());
+                tableModel.setValueAt(task.getStartTime(), selectedRow, 2);
+                tableModel.setValueAt(task.getEffectiveTime(), selectedRow, 4);
+                task.setFirstTime(false);
+
+            }
+            else if (!task.isFirstTime() && task.getStatus() == Status.PAUSE) {
+                task.setStatus(Status.PLAY);
+                task.setFirstTime(false);
+                //System.out.println("Duration: " + Duration.between(start, end));
+                //System.out.println("Effective: " + task.getEffectiveTime());
+                task.setEffectiveTime(task.getEffectiveTime().plusMinutes(Duration.between(LocalTime.now(), task.getStartTime()).toMinutes()));
+                tableModel.setValueAt(task.getEffectiveTime(), selectedRow, 4);
+            }
+            else if (!task.isFirstTime() && task.getStatus() == Status.PLAY) {
+                task.setStatus(Status.PAUSE);
+                task.setFirstTime(false);
+                task.setStartTime(LocalTime.now());
+            }
+        }
+        else
+            JOptionPane.showMessageDialog(null, "Select a valid task");
     }
 
     public void stop() {
-        System.out.println("Stop task");
+        int selectedRow = taskTable.getSelectedRow();
+        int rowCount = taskTable.getRowCount();
+
+        if(selectedRow >= 0 && rowCount >= 1) {
+            Task task = find((int)taskTable.getValueAt(selectedRow, 0));
+
+            if (!task.isFirstTime() && task.getStatus() == Status.PAUSE) {
+                task.setStatus(Status.PLAY);
+                task.setFirstTime(true);
+                task.setEndTime(LocalTime.now());
+                tableModel.setValueAt(task.getEndTime(), selectedRow, 3);
+            }
+            else if (!task.isFirstTime() && task.getStatus() == Status.PLAY) {
+                task.setStatus(Status.PLAY);
+                task.setFirstTime(true);
+                task.setEndTime(LocalTime.now());
+                tableModel.setValueAt(task.getEndTime(), selectedRow, 3);
+                task.setEffectiveTime(task.getEffectiveTime().plusMinutes(Duration.between(LocalTime.now(), task.getStartTime()).toMinutes()));
+                tableModel.setValueAt(task.getEffectiveTime(), selectedRow, 4);
+            }
+
+            updateTask(task);
+        }
+        else
+            JOptionPane.showMessageDialog(null, "Select a valid task");
     }
 
     /**
@@ -88,9 +157,12 @@ public class TableController {
     public void refreshTable(int option) {
         if (option == 1) {
             Task task = new Task().getLast();
+            taskList.add(task);
             tableModel.addRow(new Object[]{task.getId(), task.getName(), dtf.format(task.getStartTime()), dtf.format(task.getEndTime()), dtf.format(task.getEffectiveTime())});
+            Bitacora.addMessage(task.getName());
         }
         else if (option == 2) {
+            taskList.removeIf(obj -> obj.getId() == (int)tableModel.getValueAt(taskTable.getSelectedRow(), 0));
             taskTable.getCellEditor().stopCellEditing();
             tableModel.removeRow(taskTable.getSelectedRow());
             taskTable.repaint();
