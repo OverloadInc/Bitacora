c9dc95c4e8f1be7290593aafa079aa351ff56200
Overload-Inc <overload.inc.mx@gmail.com>
2022-08-21 15:44:48 -0500

    Update Status enumerator's JavaDoc comments

---
+++ src/over/core/model/pojo/Status.java
@@ -0,0 +1,10 @@
+package over.core.model.pojo;
+
+/**
+ * <code>Status</code> enumerator class to establish the status of a specific component.
+ * @author Overload Inc.
+ * @version 1.0, 01 Jul 2022
+ */
+public enum Status {
+    PLAY, PAUSE
+}
